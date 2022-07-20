b7e4fb745b772edf5bffc3552246d570ed5c589b
Karol Ismael <ismael.patricio@colimasoft.com>
2022-07-20 12:03:52 -0500

    migracion a produccion de kiosko

--- app/src/main/java/com/example/kiosko_model/network/ComponentesInterceptor.kt
+++ app/src/main/java/com/example/kiosko_model/network/ComponentesInterceptor.kt
@@ -5,8 +5,8 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class ComponentesInterceptor: Interceptor {
-    private val Username = "Admin"
-    private val Password = "password"
+    private val Username = "AdminKiosko"
+    private val Password = "T1endA5K105k0"
     private val credential = Credentials.basic(Username,Password)
 
 
