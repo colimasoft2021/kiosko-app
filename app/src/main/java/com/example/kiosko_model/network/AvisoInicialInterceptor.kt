759ba2cac4c4cb1ef05dc4537a181251c4176ca9
Karol Ismael <ismael.patricio@colimasoft.com>
2022-08-02 09:55:55 -0500

    cambio de endpoint a ambiente de kiosko por milesima vez

--- app/src/main/java/com/example/kiosko_model/network/AvisoInicialInterceptor.kt
+++ app/src/main/java/com/example/kiosko_model/network/AvisoInicialInterceptor.kt
@@ -5,8 +5,10 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class AvisoInicialInterceptor : Interceptor {
-    private val Username = "AdminKiosko"
-    private val Password = "T1endA5K105k0"
+    private val Username = "Admin"
+    private val Password = "password"
+//    private val Username = "AdminKiosko"
+//    private val Password = "T1endA5K105k0"
 
     private val credential = Credentials.basic(Username, Password)
 
