d28780a7390064dbd0233031d05df59c65a1fa7f
Karol Ismael <ismael.patricio@colimasoft.com>
2022-08-02 12:14:48 -0500

    se creo la apk release con firma, se modificaron los endpoints a ambiente de kiosko, se borro una imagen .png que causaba conflictos porque no existia o estaba dañada y se remplazo esa imagen por el icono otra vez, y se volvio a crear el icono por si las dudas

--- app/src/main/java/com/example/kiosko_model/util/Constants.kt
+++ app/src/main/java/com/example/kiosko_model/util/Constants.kt
@@ -7,18 +7,18 @@ class Constants {
         const val LOGIN_BASE_URL = "https://accesossicom.mikiosko.mx/api/"
 
 
-        /*        const val COMPONENTES_BASE_URL = "https://cmstienda.mikiosko.mx/Modulos/"
+        const val COMPONENTES_BASE_URL = "https://cmstienda.mikiosko.mx/Modulos/"
 
-                const val VIDEO_BASE_URL = "https://cmstienda.mikiosko.mx/Componentes/"
+        const val VIDEO_BASE_URL = "https://cmstienda.mikiosko.mx/Componentes/"
 
 
-                const val REGISTRO_USER_URL = "https://cmstienda.mikiosko.mx/Usuarios/"*/
-        const val COMPONENTES_BASE_URL = "https://kiosko-test.col-dev.net/Modulos/"
+        const val REGISTRO_USER_URL = "https://cmstienda.mikiosko.mx/Usuarios/"
+/*        const val COMPONENTES_BASE_URL = "https://kiosko-test.col-dev.net/Modulos/"
 
         const val VIDEO_BASE_URL = "https://kiosko-test.col-dev.net/Componentes/"
 
 
-        const val REGISTRO_USER_URL = "https://kiosko-test.col-dev.net/Usuarios/"
+        const val REGISTRO_USER_URL = "https://kiosko-test.col-dev.net/Usuarios/"*/
 
     }
 }
